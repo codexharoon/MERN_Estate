fd2bffff29f60a02e0a6e5fbcebc199eea459929
Muhammad Haroon <theharoonsocial@gmail.com>
2024-02-02 16:26:08 +0500

    created get all listings api route

--- server/controller/listing.js
+++ server/controller/listing.js
@@ -1,4 +1,5 @@
 import LISTING from "../models/listing.js";
+import { errorHandler } from "../utils/error-handler.js";
 
 export const create = async (req, res, next) => {
   try {
@@ -10,5 +11,24 @@ export const create = async (req, res, next) => {
     });
   } catch (err) {
     next(err);
+  }
+};
+
+export const getListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(
+      errorHandler("401", "You are not authorized to view the listings")
+    );
+  }
+
+  try {
+    const listings = await LISTING.find({ userRef: req.params.id });
+    if (!listings) {
+      return next(errorHandler("404", "No listings found"));
+    }
+
+    res.status(200).json(listings);
+  } catch (e) {
+    return next(e);
   }
 };
--- server/routes/listing.js
+++ server/routes/listing.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { create } from "../controller/listing.js";
+import { create, getListings } from "../controller/listing.js";
 import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router();
 
+router.get("/all/:id", verifyToken, getListings);
 router.post("/create", verifyToken, create);
 
 export default router;
