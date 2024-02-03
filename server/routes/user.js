9387328848b060d676448a0b2971ca66dfb38d6d
Muhammad Haroon <theharoonsocial@gmail.com>
2024-02-03 17:11:51 +0500

    created landlord functionality in listing page

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 import { verifyToken } from "../utils/verifyToken.js";
-import { update, deleteUser } from "../controller/user.js";
+import { update, deleteUser, getUser } from "../controller/user.js";
 
 router.put("/update/:id", verifyToken, update);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/get/:id", verifyToken, getUser);
 
 export default router;
